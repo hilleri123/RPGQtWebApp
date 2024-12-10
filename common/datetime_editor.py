823c4750e56aeda4e7f7f5bec2fa104d8e2bf487
shurik <hilleri123@gmail.com>
2024-12-10 16:33:29 +0300

    a

--- common/datetime_editor.py
+++ common/datetime_editor.py
@@ -13,36 +13,43 @@ class DateTimeEditWidget(QWidget):
         # Основной виджет QDateTimeEdit
         self.date_time_edit = QDateTimeEdit()
         self.date_time_edit.setReadOnly(True)  # Устанавливаем режим только для чтения
-        # self.date_time_edit.setDateTime(QDateTime.currentDateTime())  # Устанавливаем текущее время
         self.date_time_edit.dateTimeChanged.connect(self.dateTimeChanged)
         self.date_time_edit.setDisplayFormat("dd/MM/yyyy HH:mm")
+        self.date_time_edit.setMinimumSize(150, 30)  # Минимальный размер для QDateTimeEdit
 
         # Кнопки управления
         self.add_hour_button = QPushButton("+h")
         self.subtract_hour_button = QPushButton("-h")
         self.add_day_button = QPushButton("+d")
         self.subtract_day_button = QPushButton("-d")
-        # self.sync_button = QPushButton("Sync")
+
+        # Устанавливаем минимальные размеры кнопок
+        button_size = (40, 30)  # Ширина и высота кнопок
+        for button in [self.add_hour_button, self.subtract_hour_button, self.add_day_button, self.subtract_day_button]:
+            button.setMinimumSize(*button_size)
+            button.setMaximumSize(*button_size)
 
         # Подключаем кнопки к методам
         self.add_hour_button.clicked.connect(self.add_hour)
         self.subtract_hour_button.clicked.connect(self.subtract_hour)
         self.add_day_button.clicked.connect(self.add_day)
         self.subtract_day_button.clicked.connect(self.subtract_day)
-        # self.sync_button.clicked.connect(self.emit_sync_signal)
 
         # Расположение кнопок и виджета
         layout = QVBoxLayout()
+        layout.setContentsMargins(5, 5, 5, 5)  # Уменьшаем отступы макета
+        layout.setSpacing(5)  # Уменьшаем расстояние между элементами
+
         layout.addWidget(self.date_time_edit)
 
         button_layout = QHBoxLayout()
+        button_layout.setSpacing(2)  # Уменьшаем расстояние между кнопками
         button_layout.addWidget(self.add_day_button)
         button_layout.addWidget(self.add_hour_button)
         button_layout.addWidget(self.subtract_hour_button)
         button_layout.addWidget(self.subtract_day_button)
-        
+
         layout.addLayout(button_layout)
-        # layout.addWidget(self.sync_button)
 
         self.setLayout(layout)
 
@@ -75,3 +82,4 @@ class DateTimeEditWidget(QWidget):
     def setDateTime(self, dt):
         if dt is not None:
             return self.date_time_edit.setDateTime(dt)
+
